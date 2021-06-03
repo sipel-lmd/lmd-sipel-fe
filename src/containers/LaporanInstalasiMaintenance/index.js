d3b9f64463f35df4787ad0361057d7bce9738893
hannajtm <hanna14jtm@gmail.com>
2021-06-03 12:38:58 +0700

    feat: preview and download fixed

--- src/containers/LaporanInstalasiMaintenance/index.js
+++ src/containers/LaporanInstalasiMaintenance/index.js
@@ -344,10 +344,11 @@ class LaporanInstalasiMaintenance extends Component {
     // Apabila jenis file adalah pdf, maka url preview yang digunakan
     // Apabila jenis file selain pdf, maka url download yang digunakan
     getUrl(report){
+        const BASE_URL = "http://localhost:2020/report/";
         if(report.fileType === "application/pdf"){
-            return report.urlFile+"/preview";
+            return BASE_URL+report.reportName+"/preview";
         }else{
-            return report.urlFile;
+            return BASE_URL+report.reportName;
         }
     }
 
