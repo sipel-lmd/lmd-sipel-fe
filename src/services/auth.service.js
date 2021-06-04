6a4b14cd89ab4bdcbdff01f10fa121d52ab2df02
hannajtm <hanna14jtm@gmail.com>
2021-06-05 02:08:10 +0700

    path fixed

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://propen-a01-sipel.herokuapp.com/auth/";
+const API_URL = "https://propen-a01-sipel.herokuapp.com/api/auth/";
 
 class AuthService {
   login(username, password) {
