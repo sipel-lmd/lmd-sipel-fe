02546277114d5d6415d5674300e22712bd5164ca
hannajtm <60378512+hannajtm@users.noreply.github.com>
2021-06-05 00:35:31 +0700

    update index.js (#15)
    * feat: initial commit branch feat/penugasan-periodeKontrak-laporan
    
    * feat: update .eslintcache
    
    * feat: add api backend link
    
    * feat: update API backend link
    
    * change apiconfig
    
    * feat: handle if ordersverified is empty
    
    * feat: fixed conflix merge after pull from feat/penugasan-periodeKontrak-Laporan
    
    * feat: preview and download fixed
    
    * feat: change local base url to sipel url
    
    * feat: integration
    
    * install validator
    
    * add serviceworker
    
    * update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -17,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
