d46367f09d38f3253f62a562e38f2616b82d6075
hannajtm <hanna14jtm@gmail.com>
2021-06-05 00:34:00 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -17,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
