20f17ab909b77f47aab85ee6ea037099cf82b6bc
hannajtm <hanna14jtm@gmail.com>
2021-06-05 02:01:27 +0700

    update auth.service

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const ORDER_NOT_VERIFIED_API_BASE_URL = "http://localhost:2020/api/v1/order-verification";
-const ORDER_DETAILS_API_BASE_URL = "http://localhost:2020/api/v1/order-details";
-const ORDER_VERIFICATION_API_BASE_URL = "http://localhost:2020/api/v1/verification"
+const ORDER_NOT_VERIFIED_API_BASE_URL = "https://propen-a01-sipel.herokuapp.com/v1/order-verification";
+const ORDER_DETAILS_API_BASE_URL = "https://propen-a01-sipel.herokuapp.com/v1/order-details";
+const ORDER_VERIFICATION_API_BASE_URL = "https://propen-a01-sipel.herokuapp.com/api/v1/verification"
 
 class OrderService {
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:2020/api/test/';
-const API_URL_FILE = 'http://localhost:2020/api/report/';
+const API_URL = 'https://propen-a01-sipel.herokuapp.com/api/test/';
+const API_URL_FILE = 'https://propen-a01-sipel.herokuapp.com/api/report/';
 
 class UserService {
   getPublicContent() {
